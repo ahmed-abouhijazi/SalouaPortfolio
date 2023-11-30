18cbdb51246be05c958f1cef9718e0e64df303e5
ABOUHIJAZI <sabir.ahmed2402@gmail.com>
2023-11-30 13:54:45 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -3,31 +3,33 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const gltfContainer = document.getElementById('gltfContainer');
-let  mixer, clock;
+let mixer, clock;
 
 // Create a scene
 const scene = new THREE.Scene();
 
 // Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, gltfContainer.clientWidth / gltfContainer.clientHeight, 0.1, 1000);
 camera.position.z = 5;
 
 // Instantiate a new renderer and set its size
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-//renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setSize(450, 550); 
-
-// Add the renderer to the DOM
+renderer.setSize(gltfContainer.clientWidth, gltfContainer.clientHeight);
 document.getElementById("gltfContainer").appendChild(renderer.domElement);
+
+// OrbitControls setup
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = true;
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.maxPolarAngle = Math.PI / 2;
 
 // Keep the 3D object on a global variable so we can access it later
 let object;
 
-// OrbitControls allow the camera to move around the scene
-let controls;
-
 // Set which object to render
-let objToRender = 'gear';
+let objToRender = 'engine';
 
 // Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
@@ -47,11 +49,13 @@ loader.load(
         }
 
         object = gltf.scene;
-        object.scale.set(1.3,0.7,0.7);
-        
+        // Adjust the scale to make it smaller
+        object.scale.set(0.04, 0.04, 0.04);
 
-        // Set initial position, rotation, or other properties if needed
-        object.position.set(0.5, 0.5, 0.2);
+        // Center the object within the container
+        const box = new THREE.Box3().setFromObject(object);
+        const center = box.getCenter(new THREE.Vector3());
+        object.position.sub(center);
 
         scene.add(object);
 
@@ -71,22 +75,22 @@ loader.load(
     }
 );
 
+window.addEventListener('resize', () => {
+    camera.aspect = gltfContainer.clientWidth / gltfContainer.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(gltfContainer.clientWidth, gltfContainer.clientHeight);
+    // Dispose of the existing controls
+    controls.dispose();
+});
+
 // Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-topLight.position.set(500, 500, 500) // top-left-ish
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
 const ambientLight = new THREE.AmbientLight(0x333333, 1);
 scene.add(ambientLight);
-
-// OrbitControls setup
-controls = new OrbitControls(camera, renderer.domElement);
-controls.enableZoom = false; // Disable zoom
-controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-controls.dampingFactor = 0.25;
-controls.screenSpacePanning = false;
-controls.maxPolarAngle = Math.PI / 2;
 
 // Add animation loop
 const animate = () => {
@@ -100,7 +104,6 @@ const animate = () => {
     controls.update();
     renderer.render(scene, camera);
 };
-
 
 clock = new THREE.Clock();
 // Initial call to the animate function
